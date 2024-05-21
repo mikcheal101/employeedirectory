27cb65d678e02291b39c7112aac497f297057a36
mikcheal101 <hirekaanmicheal@gmail.com>
2024-05-21 07:26:05 +0100

    removed controller and service to use magic-api

--- src/main/java/com/mikkytrionze/employeedirectory/service/PersonService.java
+++
@@ -1,7 +0,0 @@
-package com.mikkytrionze.employeedirectory.service;
-
-import com.mikkytrionze.employeedirectory.DAO.IPersonDAO;
-
-public abstract class PersonService implements IPersonService {
-    protected IPersonDAO _IPersonDAO;
-}
