0389de07d4851d307b1b806b0e3e59d0689833e4
mikcheal101 <hirekaanmicheal@gmail.com>
2024-05-21 07:51:39 +0100

    removed controller and service

--- src/main/java/com/mikkytrionze/employeedirectory/repo/EmployeeRepository.java
+++ src/main/java/com/mikkytrionze/employeedirectory/repo/EmployeeRepository.java
@@ -1,7 +1,10 @@
 package com.mikkytrionze.employeedirectory.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import com.mikkytrionze.employeedirectory.entity.Employee;
 
+@RepositoryRestResource(path = "employee")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
